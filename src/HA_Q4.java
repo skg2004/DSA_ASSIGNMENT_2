d510bb257bfe3c4c04c8b8914964b0223792dea6
SWAYAM KUMAR GOUDA <github20042004@gmail.com>
2023-07-04 20:04:38 +0530

    updated

--- src/HA_Q4.java
+++ src/HA_Q4.java
@@ -1,33 +1,44 @@
-class Point{
-    Point(){
-    }
-    int x ,y;
-    Point(int  x, int y){
+import java.util.Scanner;
+
+class PointType{
+    double x,y;
+    PointType(double x ,double y){
         this.x=x;
         this.y=y;
     }
-    void display(){
-        System.out.println("x coordinate: "+x);
-        System.out.println("y coordinate: "+y);
-    }
 }
-class Circle extends Point{
-    double radius;
-    Circle(double radius){
-        this.radius=radius;
+class circleType extends PointType{
+    double p,q;
+    circleType(double x ,double y ,double p , double q){
+        super(x,y);
+        this.p=p;
+        this.q=q;
     }
-    void calculate(){
-        double area = 3.14*radius*radius;
-        double circumference = 2*3.14*radius;
-        System.out.println("Area of the circle is "+area);
-        System.out.println("circumference is "+circumference);
+    double Calc_radius(){
+        double radius=Math.sqrt(Math.pow(p-x,2)+Math.pow(q-y,2));
+        return radius;
+    }
+    double area(){
+        double area =3.14*Calc_radius()*Calc_radius();
+        return area;
+    }
+    double circumference(){
+        double circumference = 2*3.14*Calc_radius();
+        return circumference;
     }
 }
 public class HA_Q4 {
     public static void main(String[] args) {
-        new Point(3,5);
-       Circle obj1 = new Circle(2.5);
-       obj1.calculate();
-        obj1.display();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a point x,y: ");
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        System.out.println("Enter center of the circle p,q: ");
+        double p =sc.nextDouble();
+        double q = sc.nextDouble();
+        circleType obj = new circleType(x,y,p,q);
+        double ans = obj.area();
+        System.out.println("Area of the circle is: "+ans);
+        System.out.println("Circumference of the circle is: "+obj.circumference());
     }
 }
